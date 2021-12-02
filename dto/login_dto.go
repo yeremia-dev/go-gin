3b7ceff8019305175e8aa95108c760fb88b7526f
Julio Panjaitan <panjaitanjulio22@gmail.com>
2021-12-02 15:36:11 +0700

    [MODIFY] add some quotation mars

--- dto/login_dto.go
+++ dto/login_dto.go
@@ -3,5 +3,5 @@ package dto
 //LoginDTO is used for modeling data when client doing login
 type LoginDTO struct {
 	Email    string `json:"email" form:"email" binding:"required" validate:"email"`
-	Password string `json:"password form:"password" binding:"required" validate:"min:6"`
+	Password string `json:"password" form:"password" binding:"required" validate:"min:6"`
 }
