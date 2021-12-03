377d34fc9f324d3ef1d31f808755979acf3c9d57
Julio Panjaitan <panjaitanjulio22@gmail.com>
2021-12-03 21:26:40 +0700

    [ADD] JWT middleware for auth

---
+++ middleware/jwt_auth.go
@@ -0,0 +1,33 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
+	"github.com/yeremia-dev/go-gin/helper"
+	"github.com/yeremia-dev/go-gin/service"
+)
+
+func AuthorizeJWT(jwtService service.JWTService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			response := helper.BuildErrorResponse("Failed to process request", "No token found", nil)
+			c.AbortWithStatusJSON(http.StatusBadRequest, response)
+			return
+		}
+		token, err := jwtService.ValidateToken(authHeader)
+		if token.Valid {
+			claims := token.Claims.(jwt.MapClaims)
+			log.Println("Claim[user_id]: ", claims["user_id"])
+			log.Println("Claims[issuer]: ", claims["issuer"])
+		} else {
+			log.Println(err)
+			response := helper.BuildErrorResponse("Token is not valid", err.Error(), nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+		}
+
+	}
+}
