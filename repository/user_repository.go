53cddf97b756052e47a7792bd76beefff2ee28e1
Julio Panjaitan <panjaitanjulio22@gmail.com>
2021-12-03 21:53:27 +0700

    [ADD] User Repository

---
+++ repository/user_repository.go
@@ -0,0 +1,77 @@
+package repository
+
+import (
+	"log"
+
+	"github.com/yeremia-dev/go-gin/entity"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	InsertUser(user entity.User) entity.User
+	Updateuser(user entity.User) entity.User
+	VerifyCredential(email string, password string) interface{}
+	IsDuplicateEmail(email string) (tx *gorm.DB)
+	FindByEmail(email string) entity.User
+	ProfileUser(userId string) entity.User
+}
+
+type userRepository struct {
+	connection *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &userRepository{
+		connection: db,
+	}
+}
+
+func (rep *userRepository) InsertUser(user entity.User) entity.User {
+
+	user.Password = hashAndSalt([]byte(user.Password))
+	rep.connection.Save(&user)
+	return user
+
+}
+
+func (rep *userRepository) Updateuser(user entity.User) entity.User {
+	user.Password = hashAndSalt([]byte(user.Password))
+	rep.connection.Save(&user)
+	return user
+}
+
+func (rep *userRepository) VerifyCredential(email string, password string) interface{} {
+	var user entity.User
+	ress := rep.connection.Where("email = ?", email).Take(&user)
+	if ress.Error == nil {
+		return user
+	}
+	return nil
+}
+
+func (rep *userRepository) IsDuplicateEmail(email string) (tx *gorm.DB) {
+	var user entity.User
+	return rep.connection.Where("email = ?", email).Take(&user)
+}
+
+func (rep *userRepository) FindByEmail(email string) entity.User {
+	var user entity.User
+	rep.connection.Where("email = ?", email).Take(&user)
+	return user
+}
+
+func (rep *userRepository) ProfileUser(userId string) entity.User {
+	var user entity.User
+	rep.connection.Find(&user, userId)
+	return user
+}
+
+func hashAndSalt(pwd []byte) string {
+	hash, err := bcrypt.GenerateFromPassword(pwd, bcrypt.MinCost)
+	if err != nil {
+		log.Println(err)
+		panic("Failed to hash a password")
+	}
+	return string(hash)
+}
