abc3e13b82e521ed2576cff333f0873cbb42e612
Julio Panjaitan <panjaitanjulio22@gmail.com>
2021-12-10 13:45:09 +0700

    [ADD] Auth service

---
+++ service/auth_service.go
@@ -0,0 +1,75 @@
+package service
+
+import (
+	"log"
+
+	"github.com/mashingan/smapping"
+	"github.com/yeremia-dev/go-gin/dto"
+	"github.com/yeremia-dev/go-gin/entity"
+	"github.com/yeremia-dev/go-gin/repository"
+	"golang.org/x/crypto/bcrypt"
+)
+
+//AuthService is a contract about something that this service can do
+type AuthService interface {
+	VerifyCredential(email string, password string) interface{}
+	CreateUser(user dto.UserCreateDTO) entity.User
+	FindByEmail(email string) entity.User
+	IsDuplicateEmail(email string) bool
+}
+
+type authService struct {
+	userRepository repository.UserRepository
+}
+
+//NewAuthService is used to initialize the auth service
+func NewAuthService(userRep repository.UserRepository) AuthService {
+	return &authService{
+		userRepository: userRep,
+	}
+}
+
+//VerifyCredential is used to verify the logged in user
+func (service *authService) VerifyCredential(email string, password string) interface{} {
+	res := service.userRepository.VerifyCredential(email, password)
+	if v, ok := res.(entity.User); ok {
+		comparedPassword := comparePassword(v.Password, []byte(password))
+		if v.Email == email && comparedPassword {
+			return res
+		}
+		return false
+	}
+	return false
+}
+
+//CreateUser service is used to access the InsertUser method on repository to store the new user data
+func (service *authService) CreateUser(user dto.UserCreateDTO) entity.User {
+	userToCreate := entity.User{}
+	err := smapping.FillStruct(&userToCreate, smapping.MapFields(&user))
+	if err != nil {
+		log.Fatalf("Failed map %v", err)
+	}
+	res := service.userRepository.InsertUser(userToCreate)
+	return res
+}
+
+//FindByEmail service is used to check an email does it exist or not
+func (service *authService) FindByEmail(email string) entity.User {
+	return service.userRepository.FindByEmail(email)
+}
+
+//IsDuplicateEmail is used to check an email before it stored on db
+func (service *authService) IsDuplicateEmail(email string) bool {
+	res := service.userRepository.IsDuplicateEmail(email)
+	return !(res.Error == nil)
+}
+
+func comparePassword(hashedPwd string, plainPassword []byte) bool {
+	byteHash := []byte(hashedPwd)
+	err := bcrypt.CompareHashAndPassword(byteHash, plainPassword)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	return true
+}
