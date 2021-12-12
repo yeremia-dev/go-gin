7f7c3b9d17f7cca3fe7f5452a9531207d172f79b
Julio Panjaitan <panjaitanjulio22@gmail.com>
2021-12-12 20:32:32 +0700

    [MODIFY] UserUpdateDTO

--- dto/user_dto.go
+++ dto/user_dto.go
@@ -2,15 +2,8 @@ package dto
 
 //UserUpdateDTO is used when user updating his data
 type UserUpdateDTO struct {
-	ID       uint64 `json:"id" form:"id" binding:"required"`
+	ID       uint64 `json:"id" form:"id"`
 	Name     string `json:"name" form:"name" binding:"required"`
-	Email    string `json:"email" form:"email" binding:"required" validate:"email"`
-	Password string `json:"password,omitempty" form:"password,omitempty" validate:"min:6"`
-}
-
-//UserCreateDTO is used when user creating new data
-type UserCreateDTO struct {
-	Name     string `json:"name" form:"name" binding:"required"`
-	Email    string `json:"email" form:"email" binding:"required" validate:"email"`
-	Password string `json:"password,omitempty" form:"password,omitempty" validate:"min:6" binding:"required"`
+	Email    string `json:"email" form:"email" binding:"required,email"`
+	Password string `json:"password,omitempty" form:"password,omitempty"`
 }
